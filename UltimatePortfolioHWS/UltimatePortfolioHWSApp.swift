1e799a170ab5eaeb5f8661110475175717b7da6a
Konneki <konnekdaniel@gmail.com>
2023-05-31 20:31:22 +0200

    Initial Commit

---
+++ UltimatePortfolioHWS/UltimatePortfolioHWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioHWSApp.swift
+//  UltimatePortfolioHWS
+//
+//  Created by Daniel Konnek on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioHWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
